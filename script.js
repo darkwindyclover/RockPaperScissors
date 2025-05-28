a1927be50d7d0cf5ce060b28aa1494856e850fca
darkwindyclover <70847294+darkwindyclover@users.noreply.github.com>
2025-05-28 17:40:08 +0300

    Interactive buttons, js enhanced

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let controls = document.querySelector('.controls');
+let humanText = document.querySelector('.humanScore');
+let computerText = document.querySelector('.computerScore');
+let desc = document.querySelector('.desc');
 
+let hScore;
+let cScore;
 
 function getComputerChoice() {
     let choice = Math.random();
@@ -13,34 +19,46 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() { 
-    return prompt("Enter your choice:").toLowerCase();
-};
+function updateUI() {
+    humanText.textContent = `Human score: ${hScore}`;
+    computerText.textContent = `Computer score: ${cScore}`;
+}
 
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound() {
-        let computerChoice = getComputerChoice();
-        let humanChoice = getHumanChoice();
-        if (computerChoice == humanChoice) {
-            console.log(`It's draw. ${humanChoice} and ${computerChoice} are the same.`);
+    hScore = 0;
+    cScore = 0;
+    controls.addEventListener('click', (e) => playRound(e.target.closest('button').id));
+    
+    function playRound(h) {
+        let c = getComputerChoice();
+        console.log(`h: ${h}, c: ${c}`);
+        if (h==c) {
+            hScore++;
+            cScore++;
+            desc.textContent = `It\'s a draw!\n${h} and ${c} are the same!`;
         }
-        else if (computerChoice == "rock" && humanChoice == "paper" ||
-                computerChoice == "paper" && humanChoice == "scissors" ||
-                computerChoice == "scissors" && humanChoice == "rock") {
-                    humanScore++;
-                    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        else if ((h == 'rock' && c == 'paper') 
+            || (h == 'paper' && c == 'scissors')
+            || (h == 'scissors' && c == 'rock')) {
+            cScore++;
+            desc.textContent = `You lose!\n${h} doesn't beat ${c}!`;
         }
         else {
-            computerScore++;
-            console.log(`You lose! ${humanChoice} doesn't beat ${computerChoice}`);
+            hScore++;
+            desc.textContent = `You win!\n${h} beats ${c}!`;
         }
-    } 
-
-    for (let i = 0; i < 5; i++) {
-        playRound();
+        updateUI();
+        if (hScore == 5 || cScore == 5) {
+            desc.style['font-weight'] = 'bold';
+            let winner = 
+            hScore < cScore ? 'Computer'
+            : hScore > cScore ? 'Human'
+            : 'No one'
+            desc.textContent = `This is the end! ${winner} wins!`;
+            Array.from(document.querySelectorAll('.controls button')).forEach((x) => x.disabled = true);
+        }
     }
-    console.log(`Human score: ${humanScore}, computer score: ${computerScore}`);
+
 }
+
+playGame();
